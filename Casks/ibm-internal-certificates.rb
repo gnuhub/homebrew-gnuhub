224aa50b68b5e4d8445e95fe6af3c88d58f536ca
gnuhub <gnuhub@gmail.com>
2014-11-14 19:53:56 +0800

    add ibm-internal-certificates

---
+++ Casks/ibm-internal-certificates.rb
@@ -0,0 +1,6 @@
+class IbmInternalCertificates < Cask
+  version '0.1'
+  sha256 '41ba01b3fc4183b58c084ad311457b98feaa6ba23d7cd267893675b9de5e2a7f'
+  url 'http://w3.mac.ibm.com/software/IBMinternalCertificates.dmg'
+  pkg 'IBMcacerts_IBMinternalCertificates.pkg'
+end
--- README.md
+++ README.md
@@ -7,4 +7,5 @@ brew cask install sublime3
 brew cask install gnuhub-rdc
 brew cask install sametime
 brew cask install ibm-notes
+brew cask install ibm-internal-certificates
 ```
