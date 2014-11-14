9c36eb3605ea2760c85d3e37c99053cfc1acc59e
gnuhub <gnuhub@gmail.com>
2014-11-14 19:49:15 +0800

    add ibm-notes

---
+++ Casks/ibm-notes.rb
@@ -0,0 +1,6 @@
+class IbmNotes < Cask
+  version '9.0.1'
+  sha256 'f2055e09cfc8de3a1db6e55d8debf958bb11bb77163f3954e69ac3d24d969be9'
+  url 'http://w3.mac.ibm.com/software/Notes901_MAC_Slipstream_English.dmg'
+  pkg 'IBM Notes Installer.mpkg'
+end
--- README.md
+++ README.md
@@ -6,4 +6,5 @@ brew update
 brew cask install sublime3
 brew cask install gnuhub-rdc
 brew cask install sametime
+brew cask install ibm-notes
 ```
