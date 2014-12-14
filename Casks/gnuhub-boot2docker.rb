5f2cdff2ad224a4b7ec0c3c201bddc522d20f1f0
gnuhub <gnuhub@gmail.com>
2014-12-14 23:48:23 +0800

    auto default message

--- Casks/gnuhub-boot2docker.rb
+++ Casks/gnuhub-boot2docker.rb
@@ -1,8 +1,6 @@
 class GnuhubBoot2docker < Cask
   version '1.3.2'
   sha256 '76015f0f7bed79989bed7f3bf7597d39ec2efaa93274ce9c5e124f0ca8f31ac6'
-
   url 'http://download.sf.net/gnuhub/Boot2Docker-1.3.2.pkg'
-  
   pkg 'Boot2Docker-1.3.2.pkg'
 end
