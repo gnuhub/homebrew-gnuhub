f44d5c2991d5e79ef17882838f10eb4493a0fd73
gnuhub <gnuhub@gmail.com>
2014-12-15 14:50:24 +0800

    [BB-33]

---
+++ Casks/gnuhub-java6.rb
@@ -0,0 +1,6 @@
+cask :v1 => "gnuhub-java6" do
+  version '2014-001'
+  sha256 '97bc9b3c47af1f303710c8b15f2bcaedd6b40963c711a18da8eac1e49690a8a0'
+  url 'http://download.sf.net/gnuhub/JavaForOSX2014-001.dmg'
+  pkg 'JavaForOSX.pkg'
+end
