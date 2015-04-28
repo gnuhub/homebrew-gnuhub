8c9287439d6df4b7cecc0bf5ffe8591e11692915
gnuhub <gnuhub@gmail.com>
2015-04-28 19:53:56 +0800

    update sublime3 to 3083

--- Casks/sublime3.rb
+++ Casks/sublime3.rb
@@ -1,7 +1,7 @@
 cask :v1 => "sublime3" do
-  version '3065'
-  sha256 '373dbfcddab7fb288351763402361cd4045f4a9510eb32f81938d0c156d161c3'
-  url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065.dmg'
+  version '3083'
+  sha256 'fe6dd8d8192fdb01988f99289e5bc1d9a4e66cf67548e144002051c23369a5ff'
+  url 'http://download.sf.net/gnuhub/sublime3083.dmg'
   app 'Sublime Text.app'
   binary 'Sublime Text.app/Contents/SharedSupport/bin/subl'
 end
