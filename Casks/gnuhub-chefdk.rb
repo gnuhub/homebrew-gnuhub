90468ba06432af48cb1baa933d2fc8a833b21078
gnuhub <gnuhub@gmail.com>
2015-10-22 10:37:23 +0800

    updata java and chefdk

--- Casks/gnuhub-chefdk.rb
+++ Casks/gnuhub-chefdk.rb
@@ -1,6 +1,6 @@
 cask :v1 => "gnuhub-chefdk" do
-  version '0.3.5-1'
-  sha256 '43994187542a86b7865840ae71ac702c9f473e893b44f6552d5040a4c0202ad1'
-  url 'http://download.sf.net/gnuhub/chefdk-0.3.5-1.dmg'
-  pkg 'chefdk-0.3.5-1.pkg'
+  version '0.9.0-1'
+  sha256 '8e1d5e79976fc8242c3a1ef2dad728316fc19048c6bc1a68410786ab495b2acb'
+  url 'http://download.sf.net/gnuhub/chefdk-0.9.0-1.dmg'
+  pkg 'chefdk-0.9.0-1.pkg'
 end
--- Casks/gnuhub-java6.rb
+++ Casks/gnuhub-java6.rb
@@ -1,6 +1,6 @@
 cask :v1 => "gnuhub-java6" do
-  version '2014-001'
-  sha256 '97bc9b3c47af1f303710c8b15f2bcaedd6b40963c711a18da8eac1e49690a8a0'
-  url 'http://download.sf.net/gnuhub/JavaForOSX2014-001.dmg'
+  version '2015-001'
+  sha256 '8f40dbf821e21410feab4d9e2e533c42518897b7cac5edfad895e91016f918fc'
+  url 'http://download.sf.net/gnuhub/javaforosx.dmg'
   pkg 'JavaForOSX.pkg'
 end
