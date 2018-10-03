ef5430242bbcc2f646f5b1db5134c06a5e5ba50c
stallman <gnuhub@icloud.com>
2018-10-03 16:29:29 +0800

    add anacanda

---
+++ Casks/gnuhub-anaconda.rb
@@ -0,0 +1,36 @@
+cask 'gnuhub-anaconda' do
+  version '5.2.0'
+  sha256 'c8089121dc89ffe8f9a0c01205bab75a112821a13d413152d6690f5eef094afa'
+
+  url "https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-#{version}-MacOSX-x86_64.sh"
+  name 'Continuum Analytics Anaconda'
+  homepage 'https://www.anaconda.com/'
+
+  depends_on macos: '>= :lion'
+  container type: :naked
+
+  installer script: {
+                      executable: "Anaconda3-#{version}-MacOSX-x86_64.sh",
+                      args:       ['-b', '-p', "#{HOMEBREW_PREFIX}/anaconda3"],
+                      sudo:       true,
+                    }
+
+  postflight do
+    set_ownership "#{HOMEBREW_PREFIX}/anaconda3"
+  end
+
+  uninstall delete: [
+                      "#{HOMEBREW_PREFIX}/anaconda3",
+                      '/Applications/Anaconda-Navigator.app',
+                    ]
+
+  zap trash: [
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.continuum.io.sfl*',
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.continuum.python.sfl*',
+             ]
+
+  caveats do
+    path_environment_variable "#{HOMEBREW_PREFIX}/anaconda3/bin"
+    files_in_usr_local
+  end
+end
